dba6508c07aa17d02a9ad95f95b0ca09e17f7c7f
jordanaverps <142835883+jordanaverps@users.noreply.github.com>
2024-10-20 21:09:36 +0530

    update changes

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGO_URI = "mongodb+srv://maddy:gB30Jo57mF1ftMOi@test01.aelnz.mongodb.net/" || process.env.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI;
 
 if (!MONGO_URI) {
   throw new Error('Please define the MONGO_URI environment variable');
